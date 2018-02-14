3460881f98a75b3aa4b0b6b169db2361d3fe369c
Amit Bhadoria <gnulib@gmail.com>
2018-02-13 20:57:17 -0800

    adding a simple block implementation

--- core/block.go
+++ core/block.go
@@ -1,13 +1,15 @@
 package core
 
 import (
-	"math/big"
+	"time"
+	"fmt"
+	"crypto/sha512"
 )
 
 // interface to implement transactions
 type Transaction interface {
 	// execute using input data, and produce and output or error
-	Execute(input interface{}) interface{}, error
+	Execute(input interface{}) (interface{}, error)
 }
 
 // interface to implement block headers
@@ -24,13 +26,87 @@ type NodeInfo interface{
 type Block interface {
 	Previous() Header
 	Miner() NodeInfo
-	Hash() Header
-	TD() *big.Int
+	Nonce() Header
+	TD() time.Duration
 	TXs() []Transaction
+	Genesis() Header
+	Since() time.Duration
+	Hash() []byte
 }
 
 // a simple blockchain spec implementation
 type SimpleBlock struct {
 	previous Header
-	miner Node
+	miner NodeInfo
+	hash []byte
+	nonce Header
+	genesis Header
+	txs	[]Transaction
+	td	  time.Duration
+	since time.Duration
+}
+
+func (b *SimpleBlock) Previous() Header {
+	return b.previous
+}
+
+func (b *SimpleBlock) Miner() NodeInfo {
+	return b.miner
+}
+
+func (b *SimpleBlock) Nonce() Header {
+	return b.nonce
+}
+
+func (b *SimpleBlock) TD() time.Duration {
+	return b.td
+}
+
+func (b *SimpleBlock) Since() time.Duration {
+	return b.since
+}
+
+func (b *SimpleBlock) Genesis() Header {
+	return b.genesis
+}
+
+func (b *SimpleBlock) TXs() []Transaction {
+	return b.txs
+}
+
+func (b *SimpleBlock) Hash() []byte {
+	return b.hash
+}
+
+type mySimpleHeader struct {
+	header string
+}
+
+func (h *mySimpleHeader) String() string {
+	return h.header
+}
+
+func newMySimpleHeader(header string) *mySimpleHeader {
+	return &mySimpleHeader {
+		header: header,
+	}
+}
+
+func (b *SimpleBlock) ComputeHash() {
+	data := ""
+	data += b.previous.String() + b.miner.Id() + b.genesis.String() + fmt.Sprintf("%d",b.td)
+	hash := sha512.Sum512([]byte(data))
+	b.hash = hash[:]
+}
+
+func NewSimpleBlock(previous Header, genesis Header, since time.Duration, miner NodeInfo) *SimpleBlock {
+	return &SimpleBlock{
+		previous: previous,
+		miner: miner,
+		genesis: genesis,
+		since: since,
+		td: time.Duration(time.Now().UnixNano()) - since,
+		txs: make([]Transaction,0),
+		hash: make([]byte,0),
+	}
 }
---
+++ core/block_test.go
@@ -0,0 +1,59 @@
+package core
+
+import (
+    "testing"
+    "time"
+    "fmt"
+    "crypto/sha512"
+    "bytes"
+)
+
+type myNode struct {}
+
+func (n *myNode) Id() string {
+	return "test node"
+}
+
+func TestNewSimpleBlock(t *testing.T) {
+	now := time.Duration(time.Now().UnixNano())
+	block := NewSimpleBlock(newMySimpleHeader("previous"), newMySimpleHeader("genesis"), now, &myNode{})
+	if block.Previous().String() != "previous" {
+		t.Errorf("Block header does not match: Expected '%s', Found '%s'", "previous", block.Previous())
+	}
+	if block.Genesis().String() != "genesis" {
+		t.Errorf("Block header does not match: Expected '%s', Found '%s'", "genesis", block.Genesis())
+	}
+	if block.Miner().Id() != "test node" {
+		t.Errorf("Block minder does not match: Expected '%s', Found '%s'", "test node", block.Previous())
+	}
+	if block.Nonce() != nil {
+		t.Errorf("Block nonce not empty: Found '%s'", block.Nonce())
+	}
+	if len(block.TXs()) != 0 {
+		t.Errorf("Block transaction list not empty: Found '%d'", len(block.TXs()))
+	}
+	if len(block.Hash()) != 0 {
+		t.Errorf("Block transaction hash not empty: Found '%s'", block.Hash())
+	}
+	if block.Since() != now {
+		t.Errorf("Block genesis time incorrect: Expected '%d' Found '%d'", now, block.Since())
+	}
+	if block.TD() > time.Second {
+		t.Errorf("Block total difficulty time incorrect: Found '%d'", block.TD())
+	}
+}
+
+func TestSimpleBlockHash(t *testing.T) {
+	now := time.Duration(time.Now().UnixNano())
+	block := NewSimpleBlock(newMySimpleHeader("previous"), newMySimpleHeader("genesis"), now, &myNode{})
+	block.ComputeHash()
+	if len(block.Hash()) != 64 {
+		t.Errorf("Block hash not 64 bytes: Found '%d'", block.Hash())
+	}
+	data := ""
+	data += block.previous.String() + block.miner.Id() + block.genesis.String() + fmt.Sprintf("%d",block.td)
+	hash := sha512.Sum512([]byte(data))
+	if bytes.Compare(block.Hash(), hash[:]) != 0 {
+		t.Errorf("Block hash incorrect: Expected '%d' Found '%d'", hash, block.Hash())
+	}
+}
